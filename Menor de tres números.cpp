5946b74435f4efa65cff29de7fa96d27fa50aa12
Al2x198 <ashurtado@alumno.montepiedra.edu.ec>
2025-10-12 09:11:51 -0500

    Add files via upload

---
+++ Menor de tres números.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a, b, c;
+    cout << "Ingrese tres numeros: ";
+    cin >> a >> b >> c;
+
+    if (a <= b && a <= c) cout << "El menor es: " << a;
+    else if (b <= a && b <= c) cout << "El menor es: " << b;
+    else cout << "El menor es: " << c;
+
+    return 0;
+}
